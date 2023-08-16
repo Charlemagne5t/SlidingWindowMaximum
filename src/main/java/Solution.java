85d139ffcbc9930468144795532ae8752a19e8c6
alek <wait82985@gmail.com>
2023-08-16 12:40:48 +0600

    initila commit. 2 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        return null;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void maxSlidingWindowTest1() {
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] expected = {3, 3, 5, 5, 6, 7};
+        int[] actual = new Solution().maxSlidingWindow(nums, k);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSlidingWindowTest2() {
+        int[] nums = {1};
+        int k = 3;
+        int[] expected = {1};
+        int[] actual = new Solution().maxSlidingWindow(nums, k);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
